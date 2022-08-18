7c1998edfedce80327c1c6cdee48ca51151b7b59
Bernard Sarfo Twumasi <bst@ddfinance.com>
2022-08-18 22:27:17 +0200

    Improvements to APIs

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,24 +7,28 @@ const instance = axios.create();
 
 // Request interceptor for API calls
 instance.interceptors.request.use(
-  async config => {
-    const accessToken = await AuthService.token();
-    config.headers = { 
-      'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-
-    }
-    return config;
-  },
-  error => {
-    if (error.response.status == 401) 
-    {
-        //Redirect User to Login
-        new AuthService().login();
-    }
-
-    Promise.reject(error)
-});
-
+    async config => {
+        const accessToken = await AuthService.token();
+        config.headers = { 
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            
+        }
+        return config;
+    },
+    error => Promise.reject(error));
+    
+instance.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status == 401) 
+        {
+            //Redirect User to Login
+            new AuthService().login();
+        }
+        
+        return Promise.reject(error);
+    });
+        
 export default instance;
